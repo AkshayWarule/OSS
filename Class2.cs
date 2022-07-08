be9a781dea60e8477bb7b096fcb5f2b6fe2a0220
SuhailHashmi <108925018+SuhailHashmi@users.noreply.github.com>
2022-07-08 15:30:25 +0530

    Now i have uploaded file

---
+++ Class2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FirstApp
+{
+    internal class Class2
+    {
+       
+        public static int count;
+
+        // data members
+        private string fName;
+        private string lName;
+        private DateTime bDate;
+        private string cNumber;
+        private string email;
+
+       
+       
+        public Class2()
+        {
+            count++;
+        }
+        public Class2(string fName, string lName, DateTime bDate, string contact, string email)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.bDate = bDate;
+            this.cNumber = contact;
+            this.email = email;
+            count++;
+        }
+
+
+        public static int GetCount()
+        {
+            return count;
+        }
+        ~Class2()
+        {
+          
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " : " + this.fName + " " +
+                this.lName + " " + this.bDate + " " +
+                this.cNumber + " " + this.email+count;
+        }
+    }
+}
